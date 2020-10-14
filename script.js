a7481484824594c8de6628abe43d123c7e4ccd1e
DilaEvdila <68858149+DilaEvdila@users.noreply.github.com>
2020-10-14 23:52:04 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function (){
+	$('#icon').click(function (){
+	$('ul').toggleClass('show')
+	})
+})
+
+function changeBodyBg(color){
+    document.body.style.background = color;
+}
+
